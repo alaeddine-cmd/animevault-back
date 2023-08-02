aadd73269d1c5c62c1e2a3b160e598f70e5d5a11
ala405 <127737904+ala405@users.noreply.github.com>
2023-08-02 14:31:23 +0200

    first commit

--- models/model.js
+++ models/model.js
@@ -1,25 +1,59 @@
 const mongoose = require('mongoose');
 
 const postSchema = new mongoose.Schema({
+  postCreator: {
+    type: mongoose.Schema.Types.ObjectId,
+    required: true,
+    ref: 'User', // Assuming your user model is named 'User'
+  },
   content: {
     required: true,
-    type: String
+    type: String,
   },
   media: {
     type: [String],
   },
-  comments: [{
-    comment: String
-  }],
+  comments: [
+    {
+      comment: String,
+      username: String,
+      userId: String,
+      reactionsToComment: {
+        type: Map,
+        of: String, // Allow string values for reactions
+        default: {},
+      },
+    },
+  ],
   reactions: {
     type: Map,
     of: Number,
-    default: new Map(),
+    default: {},
   },
   signaled: {
     type: Boolean,
-    default: false
-  }
+    default: false,
+  },
+  signaledBy: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'User',
+    },
+  ],
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+  },
+  lastReactionState: [
+    {
+      emoji: String,
+      count: Number,
+      user: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+      },
+    },
+  ],
 });
 
 const Post = mongoose.model('Post', postSchema);
--- routes/routes.js
+++ routes/routes.js
@@ -2,125 +2,273 @@ const express = require('express');
 const multer = require('multer');
 const router = express.Router();
 const Post = require('../models/model');
-const emojiRegex = require('emoji-regex');
-const { Octokit } = require('@octokit/rest');
+const User = require('../models/user');
+const sharp = require('sharp');
 const path = require('path');
-const fs = require('fs');
-const util = require('util');
-const writeFileAsync = util.promisify(fs.writeFile);
 
-const octokit = new Octokit({
-  auth: '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',
-});
 // Multer storage configuration
-const storage = multer.diskStorage({
-  destination: (req, file, cb) => {
-    cb(null, path.join(__dirname, '..', 'uploads')); // Set the destination folder
-  },
-  filename: (req, file, cb) => {
-    cb(null, file.originalname); // Use the original filename
-  }
-});
-
-// Create the multer middleware
+const storage = multer.memoryStorage();
 const upload = multer({ storage });
 
-// Get all posts
-router.get('/posts', async (req, res) => {
+//Get image of a specific post
+router.get('/posts/:id/image', async (req, res) => {
+  const { id } = req.params;
+
   try {
-    const posts = await Post.find();
-    res.json(posts);
+    const post = await Post.findById(id);
+
+    if (!post || !post.media || post.media.length === 0) {
+      return res.status(404).json({ error: 'Image not found' });
+    }
+
+    // Assuming there is only one image in the media array, retrieve the first one
+    const imageData = post.media[0];
+
+    // Convert the base64 image data to a buffer
+    const imageBuffer = Buffer.from(imageData, 'base64');
+
+    // Use sharp to identify the image type and set the appropriate file extension
+    const metadata = await sharp(imageBuffer).metadata();
+    const fileExtension = `.${metadata.format}`;
+
+    res.writeHead(200, {
+      'Content-Type': `image/${metadata.format}`,
+      'Content-Length': imageBuffer.length,
+      'Content-Disposition': `inline; filename=${id}${fileExtension}` // Set the filename for the image
+    });
+
+    // Send the image buffer as the response
+    res.end(imageBuffer);
   } catch (error) {
-    res.status(500).json({ error: 'Failed to fetch posts' });
+    console.error(error);
+    res.status(500).json({ error: 'Failed to fetch the image' });
   }
 });
 
-// Create a new pos
-
+// Create a new post
 router.post('/posts', upload.single('image'), async (req, res) => {
-  const { content } = req.body;
+  const { content, userId } = req.body;
 
   try {
     const reactions = {
       heart: 0,
       sad: 0,
       like: 0,
-      laugh: 0
+      laugh: 0,
     };
 
-    let imageURL = null;
+    let mediaData = [];
 
     if (req.file) {
-      const { buffer, originalname } = req.file;
+      // Resize the image to a smaller size (e.g., 500x500)
+      const resizedImageBuffer = await sharp(req.file.buffer).toBuffer(100);
 
-      // Move the uploaded file to the 'uploads' folder
-      const filePath = path.join(__dirname, '..', 'uploads', originalname);
-
-      fs.writeFileSync(filePath, buffer);
-
-      // Create a release on GitHub
-      octokit.repos.createRelease({
-        owner: 'alaeddine-cmd',
-        repo: 'animevault-back',
-        tag_name: 'v1.0', // Specify a tag name for the release
-      })
-        .then((release) => {
-          // Upload the image file as a release asset
-          octokit.repos.uploadReleaseAsset({
-            owner: 'alaeddine-cmd',
-            repo: 'animevault-back',
-            release_id: release.data.id,
-            name: originalname,
-            data: buffer,
-          })
-            .then((uploadAsset) => {
-              // Retrieve the download URL of the uploaded asset
-              imageURL = uploadAsset.data.browser_download_url;
-
-              // Create a post with the saved image URL
-              createPost();
-            })
-            .catch((error) => {
-              console.error(error);
-              res.status(500).json({ error: 'Failed to upload the file to GitHub' });
-            });
-        })
-        .catch((error) => {
-          console.error(error);
-          res.status(500).json({ error: 'Failed to create a release on GitHub' });
-        });
-    } else {
-      // Create a post without an image URL
-      createPost();
+      // Convert the resized image buffer to base64 data
+      const base64Image = resizedImageBuffer.toString('base64');
+      mediaData.push(base64Image);
     }
 
-    function createPost() {
-      Post.create({
-        content,
-        media: imageURL ? [imageURL] : [],
-        reactions,
-      })
-        .then((post) => {
-          res.status(201).json(post);
-        })
-        .catch((error) => {
-          console.error(error);
-          res.status(400).json({ error: 'Failed to create a post' });
-        });
-    }
+    // Create a new post with the user and media data
+    const post = new Post({
+      content,
+      media: mediaData,
+      reactions,
+      postCreator: userId, // Set the postCreator field to the userId
+    });
+
+    await post.save();
+
+    res.status(201).json(post);
   } catch (error) {
     console.error(error);
     res.status(400).json({ error: 'Failed to create a post' });
   }
 });
 
+router.post('/:postId/comment/:commentId/react', async (req, res) => {
+  const { postId, commentId } = req.params;
+  const { emoji, userId } = req.body;
+  try {
+    const post = await Post.findById(postId);
+    if (!post) {
+      return res.status(404).json({ message: 'Post not found' });
+    }
+
+    const comment = post.comments.id(commentId);
+    if (!comment) {
+      return res.status(404).json({ message: 'Comment not found' });
+    }
+
+    // Check if the comment has the reactionsToComment map or initialize it if not present
+    if (!comment.reactionsToComment) {
+      comment.reactionsToComment = new Map();
+    }
+
+    // Check if the user has already reacted to this comment
+    const userReaction = comment.reactionsToComment.get(userId);
+    if (userReaction) {
+      // User has already reacted, update the reaction
+      comment.reactionsToComment.set(userId, emoji);
+    } else {
+      // User has not reacted before, add the new reaction
+      comment.reactionsToComment.set(userId, emoji);
+    }
+
+    // Save the updated post
+    post.markModified('comments'); // Mark the 'comments' field as modified
+    await post.save();
+
+    res.status(200).json({ message: 'Reaction added to comment successfully', post });
+  } catch (error) {
+    console.error('Error adding reaction to comment:', error);
+    res.status(500).json({ message: 'Server error' });
+  }
+});
 
 
+router.get('/:postId/comments/react-count', async (req, res) => {
+  const { postId } = req.params;
+  try {
+    const post = await Post.findById(postId);
+    if (!post) {
+      return res.status(404).json({ message: 'Post not found' });
+    }
+
+    const commentReactions = [];
+    post.comments.forEach(comment => {
+      const reactionCountMap = new Map();
+      if (comment.reactionsToComment) {
+        // Count the emoji reactions for this comment
+        comment.reactionsToComment.forEach((emoji, userId) => {
+          if (reactionCountMap.has(emoji)) {
+            reactionCountMap.set(emoji, reactionCountMap.get(emoji) + 1);
+          } else {
+            reactionCountMap.set(emoji, 1);
+          }
+        });
+      }
+      commentReactions.push({
+        commentId: comment._id,
+        reactionCountMap: Object.fromEntries(reactionCountMap),
+      });
+    });
+
+    res.status(200).json({ postId, commentReactions });
+  } catch (error) {
+    console.error('Error fetching comment reactions:', error);
+    res.status(500).json({ message: 'Server error' });
+  }
+});
+
+//Get all comm + username
+
+router.get('/posts/:postId/comments', async (req, res) => {
+  const { postId } = req.params;
+
+  try {
+    const post = await Post.findById(postId).populate('comments.user', 'username');
+    if (!post) {
+      return res.status(404).json({ error: 'Post not found' });
+    }
+    const comments = post.comments.map(comment => ({
+      comment: comment.comment,
+      username: comment.user.username
+    }));
+    res.status(200).json(comments);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Failed to fetch comments' });
+  }
+});
 
 
+// Edit a comment
+router.put('/posts/:postId/comments/:commentId', async (req, res) => {
+  const { postId, commentId } = req.params;
+  const { comment } = req.body; // The updated comment content from the client
+
+  try {
+    const post = await Post.findById(postId);
+
+    if (!post) {
+      res.status(404).json({ error: 'Post not found' });
+      return;
+    }
+
+    const commentIndex = post.comments.findIndex(
+      (comment) => comment._id.toString() === commentId
+    );
+
+    if (commentIndex === -1) {
+      res.status(404).json({ error: 'Comment not found' });
+      return;
+    }
+
+    post.comments[commentIndex].comment = comment; // Update the comment content
+    await post.save();
+
+    res.status(200).json(post.comments[commentIndex]); // Send the updated comment data in the response
+  } catch (error) {
+    console.error(error);
+    res.status(400).json({ error: 'Failed to update the comment' });
+  }
+});
 
 
+// Delete a comment
+router.delete('/posts/:postId/comments/:commentId', async (req, res) => {
+  const { postId, commentId } = req.params;
 
+  try {
+    const post = await Post.findById(postId);
+
+    if (!post) {
+      res.status(404).json({ error: 'Post not found' });
+      return;
+    }
+
+    const commentIndex = post.comments.findIndex(
+      (comment) => comment._id.toString() === commentId
+    );
+
+    if (commentIndex === -1) {
+      res.status(404).json({ error: 'Comment not found' });
+      return;
+    }
+
+    post.comments.splice(commentIndex, 1);
+    await post.save();
+
+    res.sendStatus(200);
+  } catch (error) {
+    console.error(error);
+    res.status(400).json({ error: 'Failed to delete the comment' });
+  }
+});
+
+
+//Get all posts
+router.get('/posts', async (req, res) => {
+  try {
+    const posts = await Post.find();
+    res.json(posts);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Failed to fetch posts' });
+  }
+});
+
+router.get('/posts/user/:userId', async (req, res) => {
+  const { userId } = req.params;
+
+  try {
+    const posts = await Post.find({ postCreator: userId });
+    res.json(posts);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Failed to fetch posts' });
+  }
+});
 // Update
 router.put('/posts/:id', async (req, res) => {
   const { id } = req.params;
@@ -164,10 +312,9 @@ router.delete('/posts/:id', async (req, res) => {
   }
 });
 
-// Add a comment to a post
 router.post('/posts/:id/comments', async (req, res) => {
   const { id } = req.params;
-  const { comment } = req.body;
+  const { userId, username, comment } = req.body; // Extract the userId, username, and comment from the request body
 
   try {
     const post = await Post.findById(id);
@@ -177,7 +324,12 @@ router.post('/posts/:id/comments', async (req, res) => {
       return;
     }
 
-    const newComment = { comment };
+    const newComment = {
+      comment,
+      username,
+      userId, // Include the userId in the new comment
+    };
+
     post.comments.push(newComment);
     await post.save();
 
@@ -187,7 +339,6 @@ router.post('/posts/:id/comments', async (req, res) => {
     res.status(400).json({ error: 'Failed to add comment to the post' });
   }
 });
-
 
 
 
@@ -218,9 +369,9 @@ router.get('/posts/:id/reactions', async (req, res) => {
 });
 
 
-// Increment a specific reaction by 1
 router.put('/posts/:id/reactions/:reaction', async (req, res) => {
   const { id, reaction } = req.params;
+  const { userId } = req.body;
 
   try {
     const post = await Post.findById(id);
@@ -235,22 +386,47 @@ router.put('/posts/:id/reactions/:reaction', async (req, res) => {
       return;
     }
 
+    const previousReaction = post.lastReactionState.find(
+      (state) => state.user.toString() === userId
+    );
+
+    if (previousReaction) {
+      // Decrement the previous reaction count
+      post.reactions.set(previousReaction.emoji, previousReaction.count - 1);
+    }
+
+    // Increment the new reaction count
     post.reactions.set(reaction, post.reactions.get(reaction) + 1);
+
+    // Update or add the user's last reaction state
+    const newReactionState = {
+      user: userId,
+      emoji: reaction,
+      count: post.reactions.get(reaction),
+    };
+    const userReactionIndex = post.lastReactionState.findIndex(
+      (state) => state.user.toString() === userId
+    );
+    if (userReactionIndex !== -1) {
+      // Update the existing user's reaction state
+      post.lastReactionState[userReactionIndex] = newReactionState;
+    } else {
+      // Add the new user's reaction state
+      post.lastReactionState.push(newReactionState);
+    }
+
     await post.save();
 
     res.json(post.reactions);
   } catch (error) {
     console.error(error);
-    res.status(400).json({ error: 'Failed to increment reaction' });
+    res.status(400).json({ error: 'Failed to update reaction' });
   }
 });
 
-
-
-
-// Decrement a specific reaction by 1
 router.put('/posts/:id/reactions/:reaction/decrement', async (req, res) => {
   const { id, reaction } = req.params;
+  const { userId } = req.body;
 
   try {
     const post = await Post.findById(id);
@@ -267,23 +443,28 @@ router.put('/posts/:id/reactions/:reaction/decrement', async (req, res) => {
 
     // Check if the reaction count is greater than 0 before decrementing
     if (post.reactions.get(reaction) > 0) {
+      // Decrement the reaction count by 1
       post.reactions.set(reaction, post.reactions.get(reaction) - 1);
-      await post.save();
-    }
 
-    res.json(post.reactions);
+      // Save the updated post to the database
+      await post.save();
+
+      res.json({ message: 'Emoji count decremented successfully' });
+    } else {
+      // No need to decrement since the count is already 0
+      res.json({ message: 'Emoji count is already 0' });
+    }
   } catch (error) {
     console.error(error);
-    res.status(400).json({ error: 'Failed to decrement reaction' });
+    res.status(500).json({ error: 'Failed to decrement emoji count' });
   }
 });
 
 
-module.exports = router;
-
 // Signal a post
 router.post('/posts/:id/signal', async (req, res) => {
   const { id } = req.params;
+  const { userId } = req.body; // Extract the user ID from the request body
 
   try {
     const post = await Post.findById(id);
@@ -293,6 +474,7 @@ router.post('/posts/:id/signal', async (req, res) => {
       return;
     }
 
+    post.signaledBy.push(userId); // Add the user ID to the `signaledBy` array
     post.signaled = true;
     await post.save();
 
@@ -302,5 +484,67 @@ router.post('/posts/:id/signal', async (req, res) => {
     res.status(400).json({ error: 'Failed to signal the post' });
   }
 });
+
+
+
+// Create a new user account
+router.post('/users', async (req, res) => {
+  const { username, password } = req.body;
+
+  try {
+    const existingUser = await User.findOne({ username });
+
+    if (existingUser) {
+      return res.status(400).json({ error: 'Username already exists' });
+    }
+
+    const newUser = new User({ username, password });
+    await newUser.save();
+
+    res.status(201).json(newUser);
+  } catch (error) {
+    console.error(error);
+    res.status(400).json({ error: 'Failed to create user account' });
+  }
+});
+
+// User login
+router.post('/login', async (req, res) => {
+  const { username, password } = req.body;
+
+  try {
+    const user = await User.findOne({ username });
+
+    if (!user || user.password !== password) {
+      return res.status(401).json({ error: 'Invalid username or password' });
+    }
+
+    res.json({ message: 'Login successful', userIdVolo: user._id, usernameVolo: user.username }); // Include the user ID in the response
+  } catch (error) {
+    console.error(error);
+    res.status(400).json({ error: 'Failed to login' });
+  }
+});
+
+router.post('/users/username', async (req, res) => {
+  const { userId } = req.body;
+
+  try {
+    // Assuming you have a User model
+    const user = await User.findById(userId);
+
+    if (!user) {
+      res.status(404).json({ error: 'User not found' });
+      return;
+    }
+
+    const username = user.username;
+    res.json({ username });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Failed to fetch username' });
+  }
+});
+
 
 module.exports = router;
